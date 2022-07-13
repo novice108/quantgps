550a6e0b4531d754720421caca1d21f8b2e77bc8
novice108 <dshv937@meta.ua>
2022-07-13 18:36:16 +0300

    Update Benchmak-MTZ-GPS-Native_TSP.py

--- Benchmak-MTZ-GPS-Native_TSP.py
+++ Benchmak-MTZ-GPS-Native_TSP.py
@@ -439,3 +439,269 @@ for N in range(2,10):
             SOLUCIONES[jjj,2] = 0
 
         #np.savetxt("Modelo_General.txt",SOLUCIONES)
+print("SOLUCIONES ", SOLUCIONES)
+#print(len(dwave_dic))
+
+# Model GPS
+modelo = 0 ## Put 0 for SA, 1 for simulated QA, 2 for real QA
+n_samples = 300 # number of times we run the system
+
+stm2 = time()
+
+R=3
+lis_r = range(R)
+lis_n = range(0,N+2)
+
+pen = 2
+pen1 = 4
+
+# We create the variables of our model
+coef = qubovert.QUBO()
+
+## Variables x_{i,j,r,q}
+for i in lis_n:
+    for j in lis_n:
+        if not i==j:
+            for r in lis_r:
+                coef.create_var(f"x_{i}_{j}_{r}")
+nqm2 = len(lis_n)*len(lis_n)*len(lis_r)
+nqm2 = (N+2)*(N+2)*3
+
+## Restriction 1
+## A single case for each r
+lambda_1 = dist_vc*pen
+for i in lis_n:
+    for j in lis_n:
+        if not i==j:
+            for r1 in lis_r:
+                for r2 in lis_r:
+                    coef[(f"x_{i}_{j}_{r1}",f"x_{i}_{j}_{r2}")] += (lambda_1)
+            for r in lis_r:     
+                    coef[(f"x_{i}_{j}_{r}",)] += -2*(lambda_1)
+ 
+## Restriction 2
+## Must exit once from each node
+for i in range(N+1):
+    lambda_2 = pen1*np.max(dist[i,])
+    for j in lis_n:
+        if not i == j:
+            coef[(f"x_{i}_{j}_{1}",)] += -2*lambda_2 
+    for j1 in  lis_n:
+        if j1 != i:
+            for j2 in lis_n:
+                if j2!=i:
+                    coef[(f"x_{i}_{j1}_{1}",f"x_{i}_{j2}_{1}")]+= lambda_2
+
+## Restriction 3
+## Each node must be reached once
+for j in range(1,N+2):
+    lambda_3 = np.max(dist[:,j])*pen1
+    for i in range(N+1):
+        if i!=j:
+            coef[(f"x_{i}_{j}_{1}",)] += -2*lambda_3
+    for i1 in  lis_n:
+        if i1!=j:
+            for i2 in lis_n:
+                if i2!=j:
+                    coef[(f"x_{i1}_{j}_{1}",f"x_{i2}_{j}_{1}")]+= lambda_3 
+
+## Restriction 7
+## Correct order of nodes i,j
+lambda_7 = dist_vc*pen
+for i in lis_n:
+    for j in lis_n:
+        if i!=j:
+            coef[(f"x_{i}_{j}_{2}",f"x_{j}_{i}_{2}")] += lambda_7 
+            coef[(f"x_{i}_{j}_{2}",)] += -lambda_7 
+            coef[(f"x_{j}_{i}_{2}",)] += -lambda_7 
+
+## Restriction 8
+## Order relationship between nodes i,j,k
+lambda_8 = dist_vc*pen
+for i in range(1,N+1):
+    for j in range(1,N+1):
+        for k in range(1,N+1):
+            if i!=j and j!=k and i!=k:
+                        coef[(f"x_{j}_{i}_{2}",f"x_{k}_{j}_{2}")] += lambda_8
+                        coef[(f"x_{j}_{i}_{2}",f"x_{k}_{i}_{2}")] -= lambda_8
+                        coef[(f"x_{k}_{j}_{2}",f"x_{k}_{i}_{2}")] -= lambda_8
+                        coef[(f"x_{k}_{i}_{2}",)] +=  lambda_8
+
+## Objective Function
+lambda_obj = 1
+for i in lis_n:
+    for j in lis_n:
+        if i!=j:
+            coef[(f"x_{i}_{j}_{1}",)] += lambda_obj*dist[i,j]
+
+dwave_dic = {}
+for i in coef:
+    if len(i) == 1:
+        dwave_dic[(i[0],i[0])] = coef[i]
+    else:
+        dwave_dic[i] = coef[i]
+
+for jjj in range(nsim):
+
+    if modelo == 0:
+        sampleset = qubovert.sim.anneal_qubo(dwave_dic, num_anneals=n_samples)
+        solution = sampleset.best.state
+        
+    if modelo == 1:
+        sampler = SimulatedAnnealingSampler()
+        #sampler = EmbeddingComposite(DWaveSampler())
+        sampleset = sampler.sample_qubo(dwave_dic, num_reads = n_samples)
+        solution = sampleset.first.sample
+    
+    if modelo == 2:
+        sampler = EmbeddingComposite(DWaveSampler())
+        sampleset = sampler.sample_qubo(dwave_dic,num_reads = n_samples)
+        solution = sampleset.first.sample
+
+    print()
+    print("The number of qubits is",nqm2)
+    
+    ## better energy
+    #print(sampleset.first.energy)
+    ## Matriz solucion
+    mat_sol = np.zeros((N+2,N+2))
+    for i in  range(N+2):
+        for j in range(N+2):
+            if i!=j:
+                if solution[f"x_{i}_{j}_{1}"] == 1:
+                    mat_sol[i,j] = 1
+    #print(mat_sol)
+
+## We paint the proposed path
+    print()
+    print("Solution path drawing.")
+    plt.plot(puntos[:,0],puntos[:,1],'o')
+    vaux = np.array(list(range(N+2)))
+    suma_ruta = 0
+    for i in range(N+1):
+        sig_aux = mat_sol[i,:]==1
+        if np.sum(sig_aux) > 0:
+            sig = (int(vaux[sig_aux][0]))%(N+1)
+            plt.plot(puntos[(i,sig),0],puntos[(i,sig),1])
+            suma_ruta += np.floor(1000*fnorm(puntos[i,:]-puntos[sig,:]))
+    plt.show()
+    
+    ## We calculate the distance traveled
+    val_resobj = 0
+    for i in lis_n:
+        for j in lis_n:
+            if i!=j:
+                val_resobj += solution[f"x_{i}_{j}_{1}"]*lambda_obj*dist[i,j]
+    print()
+    print("The length of the solution path is",val_resobj)
+    lpm2 = val_resobj
+
+    etm2 = np.round((time()-stm2)/60,3) ## Elapsed time model 1
+    print()
+    print("Running time has been",etm2,"minutes.")
+    
+    best_energy = 0
+    ## Restriction 1
+    val_res1 = 0
+    for i in lis_n:
+        for j in lis_n:
+            if i != j:
+                for r1 in lis_r:
+                    for r2 in lis_r:
+                        val_res1 += lambda_1*solution[f"x_{i}_{j}_{r1}"]*solution[f"x_{i}_{j}_{r2}"]  
+                for r in lis_r:     
+                        val_res1 += -2*lambda_1*solution[f"x_{i}_{j}_{r}"] 
+
+    print("Constraint 1 is contributing ",val_res1, " and should contribute ",-((N+2)**2-(N+2))*lambda_1)                  
+    best_energy += val_res1
+    val_cor1 = -((N+2)**2-(N+2))*lambda_1
+    
+    ## Restriction 2
+    val_res2 = 0
+    val2_tot = 0
+    for i in range(N+1):
+        lambda_2 = pen1*np.max(dist[i,])
+        val2_tot += lambda_2
+        for j in lis_n:
+            if i != j:
+                val_res2 += -2*lambda_2 *solution[f"x_{i}_{j}_{1}"] 
+        for j1 in  lis_n:
+            if i!=j1:
+                for j2 in lis_n:
+                    if i!=j2:
+                        val_res2 += solution[f"x_{i}_{j1}_{1}"]*solution[f"x_{i}_{j2}_{1}"]*lambda_2 
+
+    print("Constraint 2 is contributing ",val_res2, " and should contribute ",-val2_tot)                
+    best_energy += val_res2                
+    val_cor2 = -val2_tot
+    
+    ## Restriction 3
+    val_res3 = 0
+    val3_tot = 0
+    for j in range(1,N+2):
+        lambda_3 = pen1*np.max(dist[:,j])
+        val3_tot += lambda_3
+        for i in range(N+1):
+            if i!=j:
+                val_res3 += -2*lambda_3*solution[f"x_{i}_{j}_{1}"] 
+        for i1 in  lis_n:
+            if i1 !=j:
+                for i2 in lis_n:
+                    if i2 !=j:
+                        val_res3 += solution[f"x_{i1}_{j}_{1}"]*solution[f"x_{i2}_{j}_{1}"]*lambda_3 
+
+    print("Constraint 3 is contributing ",val_res3, " and should contribute ",-val3_tot) 
+    val_cor3 = -val3_tot
+    best_energy += val_res3
+    
+    ## Restriction 7
+    val_res7 = 0
+    for i in lis_n:
+        for j in lis_n:
+            if i!=j:
+                val_res7 += solution[f"x_{i}_{j}_{2}"]*solution[f"x_{j}_{i}_{2}"] * lambda_7 
+                val_res7 += solution[f"x_{i}_{j}_{2}"] * (-lambda_7) 
+                val_res7 += solution[f"x_{j}_{i}_{2}"] * (-lambda_7) 
+    print("Constraint 7 is contributing ",val_res7, " and should contribute ",(-(N+2)**2+N+2)*lambda_7)      
+    val_cor7 = (-(N+2)**2+N+2)*lambda_7
+    best_energy += val_res7
+    
+    ## Restriction 8
+    val_res8 = 0
+    for i in range(1,N+1):
+        for j in range(1,N+1):
+            for k in range(1,N+1):
+                if i!=j and j!=k and i!=k:
+                            val_res8 += solution[f"x_{j}_{i}_{2}"]*solution[f"x_{k}_{j}_{2}"] * lambda_8
+                            val_res8 += -solution[f"x_{j}_{i}_{2}"]*solution[f"x_{k}_{i}_{2}"] * lambda_8
+                            val_res8 += -solution[f"x_{k}_{j}_{2}"]*solution[f"x_{k}_{i}_{2}"] * lambda_8
+                            val_res8 += solution[f"x_{k}_{i}_{2}"]*solution[f"x_{k}_{i}_{2}"] * lambda_8
+    print("Constraint 8 is contributing ",val_res8, " and should contribute ",0)      
+    val_cor8  = 0
+    best_energy += val_res8
+    
+     ## Objective Function
+    val_resobj = 0
+    for i in lis_n:
+        for j in lis_n:
+            if i != j:
+                val_resobj += solution[f"x_{i}_{j}_{1}"]*lambda_obj*dist[i,j]
+    print("The objective function is contributing ",val_resobj )                  
+    best_energy += val_resobj              
+
+    #print("Our energy is ",best_energy)
+    #print("The energy of sampleset is ",sampleset.first.energy)
+    
+    val_res_suma = val_res1+val_res2+val_res3+val_res7+val_res8
+    val_res_cor = val_cor1+val_cor2+val_cor3+val_cor7+val_cor8
+    if np.round(val_res_suma,3) == np.round(val_res_cor,3):
+        SOLUCIONES[jjj,0] = 0
+        SOLUCIONES[jjj,1] = etm2
+        SOLUCIONES[jjj,2] = lpm2
+    else:
+        SOLUCIONES[jjj,0] = 1
+        SOLUCIONES[jjj,1] = etm2
+        SOLUCIONES[jjj,2] = 0
+        
+    np.savetxt("Modelo_GPS.txt", SOLUCIONES)
+ 
